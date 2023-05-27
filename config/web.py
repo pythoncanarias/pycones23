a9caa485c07206355e5452e2c53b000b5e132220
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2023-05-27 20:06:05 +0200

    Add FAQ section
    - Including a few initial questions in EN and ES
    - The "Organization" section was moved to the footer and
      the FAQ was put it that place instead

--- config/web.py
+++ config/web.py
@@ -22,7 +22,7 @@ NAV_VALUES = [
     {"slug": "viaje", "text": "Viaje"},
     {"slug": "patrocinios", "text": "Patrocinios"},
     {"slug": "c4p", "text": "Llamado de Propuestas"},
-    {"slug": "organizacion", "text": "Organización"},
+    {"slug": "faq", "text": "Preguntas frecuentes"},
 ]
 
 # i18n
@@ -42,7 +42,7 @@ I18N_SUBSITES = {
                 {"slug": "viaje", "text": "Trip"},
                 {"slug": "patrocinios", "text": "Sponsors"},
                 {"slug": "c4p", "text": "Call for Proposals"},
-                {"slug": "organizacion", "text": "Organization"},
+                {"slug": "faq", "text": "FAQ"},
             ],
             'CRONOGRAMA':  [
                 {"fecha": "February", "desc": "We present the PyConES in Canary Islands!"},
